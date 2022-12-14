8ad98a8e09955593fcd740c617f8d600062aa3ea
kanno-y <kanno0521@gmail.com>
2022-12-14 23:12:53 +0900

    add: 解答

---
+++ src/readCsv2.ts
@@ -0,0 +1,39 @@
+type User = {
+  name: string;
+  age: number;
+  premiumUser: boolean;
+};
+
+const data: string = `
+  uhyo,26,1
+  John Smith,17,0
+  Mary Sue, 14, 1
+`;
+
+const users: User[] = [];
+
+const lines = data.split("\n");
+console.log("lines", lines);
+for (const line of lines) {
+  if (line === "") {
+    continue;
+  }
+  console.log("line", line);
+  const [name, ageString, premiumUserString] = line.split(",");
+  const age = Number(ageString);
+  const premiumUser = premiumUserString === "1";
+
+  users.push({
+    name,
+    age,
+    premiumUser,
+  });
+}
+
+for (const user of users) {
+  if (user.premiumUser) {
+    console.log(`${user.name} (${user.age})はプレミアムユーザー`);
+  } else {
+    console.log(`${user.name} (${user.age})はプレミアムユーザーではありません`);
+  }
+}
